9df6bfabe7f6dedac4092d00340752a8f0da2078
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-06-11 07:50:50 +0200

    Initial Commit

---
+++ GradientsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientsPractice
+//
+//  Created by Manyuchi, Carrington C on 2024/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
